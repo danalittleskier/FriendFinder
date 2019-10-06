5e1427723ca09d031978518c6295871f274f436d
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-10-05 19:51:09 -0600

    Updated api post route to add new friend from form and check the logic for best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,36 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
+        var totalDifference = 0;
+        var firstTime = true;
+        var matchArray = [];
+
+        friendsList.forEach(friend => {
+            for (let i = 0; i < 4; i++) {
+                    totalDifference += Math.abs(friend.scores[i] - req.body.scores[i]);    
+            }
+            console.log("total difference = "+totalDifference);
+            if(!matchArray || !matchArray.length){
+                var newMatch = {
+                    name: friend.name,
+                    photo: friend.photo,
+                    totalDifference: totalDifference
+                }
+                console.log("first match "+newMatch.name);
+                matchArray.push(newMatch);
+            }
+            else if (matchArray[matchArray.length-1].totalDifference > totalDifference){
+                var secondMatch = {
+                    name: friend.name,
+                    photo: friend.photo,
+                    totalDifference: totalDifference
+                }
+                console.log("Second match "+secondMatch.name);
+                matchArray.push(secondMatch);
+            }
+            totalDifference = 0;
+        });
         friendsList.push(req.body);
+        res.json(matchArray[matchArray.length-1]);
     });
 }
