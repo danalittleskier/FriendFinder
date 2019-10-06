6db7954f187a0b197d430872fe6059ceb812ee42
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-10-05 19:50:15 -0600

    Updated data file with more friends objects - friendsList

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,7 @@
-var friendsList = [{
+var friendsList = [
+    {
     name: "Aksel",
-    photo: "./images/aksel.jpg",
+    photo: "https://image.redbull.com/rbcom/010/2015-06-24/1331731349232_2/0012/0/66/0/4393/6499/1500/1/world-champion-alpine-skier-aksel-lund-svinda-2015.jpg",
     scores :[
         5,
         1,
@@ -13,7 +14,55 @@ var friendsList = [{
         4,
         1
       ]
-  }];
+  },
+  {
+  name: "Thor",
+  photo: "https://cdn.mamamia.com.au/wp/wp-content/uploads/2017/10/19170125/Thor-Ragnorok-Social-650x507.jpg",
+  scores :[
+      1,
+      5,
+      2,
+      5,
+      5,
+      1,
+      2,
+      4,
+      1,
+      4
+    ]
+},
+{
+    name: "Optimus Prime",
+    photo: "https://images.alphacoders.com/911/thumb-1920-91.jpg",
+    scores :[
+        3,
+        3,
+        5,
+        2,
+        4,
+        3,
+        2,
+        3,
+        2,
+        4
+      ]   
+},
+{
+    name: "Iron Man",
+    photo: "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2019%2F04%2Firon-man-suit-and-thanos-weapon-in-avengers-endgame-001.jpg?q=75&w=800&cbr=1&fit=max",
+    scores :[
+        1,
+        1,
+        5,
+        3,
+        4,
+        2,
+        3,
+        5,
+        2,
+        5
+      ] 
+}];
   
 
   module.exports = friendsList;
