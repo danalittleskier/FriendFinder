ecf89687aa26d73443b018ba007e1d10f6c9b942
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-10-07 22:54:04 -0600

    Updated routes to grab css file cause the import was failing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,21 @@
 var path = require("path");
 
-module.exports = function(app) {
- 
-    app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
-  
-    // If no matching route is found default to home
-    app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-  };
-  
+module.exports = function (app) {
+
+  app.get("/images/hearts.jpg", function (req, res) {
+    res.sendFile(path.join(__dirname, "../images/hearts.jpg"));
+  });
+
+  app.get("/css/style.css", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/css/style.css"));
+  });
+
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
